101a9a7c05224e88da9cc6af5165556233d341a5
mahdiboybird <43482094+mahdiboybird@users.noreply.github.com>
2024-12-14 17:25:20 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -34,7 +34,7 @@ let ChatID ='';
 let proxyhosts = [];
 let proxyhostsURL = '';
 let RproxyIP = 'false';
-let httpsPorts = ["2053","2083","2087","2096","8443"];
+let httpsPorts = ["2053","2083","2087","2096","8443","80","10000","433"];
 let sha224Password ;
 const regex = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|\[.*\]):?(\d+)?#?(.*)?$/;
 let proxyIPPool = [];
